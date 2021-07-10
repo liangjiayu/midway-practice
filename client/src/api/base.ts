a5d2f6bf1e0a277caa8d9b0f5907de0b1d69f6fa
LiangJiaYu <1013834609@qq.com>
2021-07-10 22:27:42 +0800

    init

--- client/src/api/base.ts
+++
@@ -1,9 +0,0 @@
-import { request } from 'umi';
-
-export async function checkOnly(params) {
-  return request('/api/base/checkOnly', { method: 'POST', data: params });
-}
-
-export async function getApiPerm(params) {
-  return request('/api/base/getApiPerm', { method: 'GET', data: params });
-}
